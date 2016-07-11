c2c166cabe242d00320c42c8de65d158ab5a462a
jorgo <einfeldt.joerg@gmail.com>
2016-07-11 14:40:11 +0200

    Added doc

--- src/main/java/hdm/stuttgart/csm/smarthome/object/Direction.java
+++ src/main/java/hdm/stuttgart/csm/smarthome/object/Direction.java
@@ -1,5 +1,11 @@
 package hdm.stuttgart.csm.smarthome.object;
 
+/**
+ * 
+ * Enum distinguished between directions on sensfloor
+ * @author Joerg Einfeldt, Thomas Derleth, Merle Hiort, Marc Stauffer
+ *
+ */
 public enum Direction {
 	INCR_X, DECR_X, INCR_Y, DECR_Y, INCR_REFERENCE_DIST, DECR_REFERENCE_DIST, UNCHANGED
 }
